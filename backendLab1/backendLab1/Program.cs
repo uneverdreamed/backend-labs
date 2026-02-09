826842225659d172a1f428411a182269326731d3
uneverdreamed <ma2005f@gmail.com>
2026-02-09 13:02:07 +0300

    Добавьте файлы проекта.

---
+++ backendLab1/backendLab1/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.Map("/", () =>
+{
+    var response = "Good job <br> <img src='https://i.pinimg.com/736x/3a/38/a0/3a38a0cce3cfc3857a3543fb3795b1fb.jpg'>";
+    return Results.Content(response, "text/html");
+
+});
+
+app.MapGet("/api/info", () =>
+{
+    return Results.Ok(new
+    {
+        appName = "backendLab1",
+        author = "Федукина Мария",
+        description = "это первая лаба по бэкенд-разработке"
+    });
+});
+
+app.MapGet("/api/time", () =>
+{
+    return Results.Ok(new
+    {
+        currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+        timezone = TimeZoneInfo.Local.DisplayName
+    });
+});
+
+app.Run();
+
