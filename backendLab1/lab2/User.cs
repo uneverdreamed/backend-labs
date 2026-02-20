3803fa5e8b2a8e02f4ea04333ca94278e3034312
uneverdreamed <ma2005f@gmail.com>
2026-02-20 10:09:07 +0300

    создана модель пользователя user

--- backendLab1/lab2/User.cs
+++ backendLab1/lab2/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Email { get; set; } = string.Empty;
+
     }
 }
