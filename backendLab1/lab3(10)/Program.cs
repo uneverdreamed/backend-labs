82f19f679eb2d2b588de0380894ed4474b6ac3ce
uneverdreamed <ma2005f@gmail.com>
2026-02-20 14:08:26 +0300

    3 лаба | реализован текстовый html ответ

--- backendLab1/lab3(10)/Program.cs
+++ backendLab1/lab3(10)/Program.cs
@@ -1,7 +1,23 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 var app = builder.Build();
+
+// html 
+app.MapGet("/api/html", () =>
+{
+    var html = """
+        <!DOCTYPE html>
+        <html lang="ru">
+        <head><meta charset="UTF-8"><title>HTML-ответ</title></head>
+        <body>
+            <h1>пупупу</h1>
+            <p>это пример HTML-ответа от сервера</p>
+        </body>
+        </html>
+        """;
+    return Results.Content(html, "text/html");
+});
 
 app.Run();
 
