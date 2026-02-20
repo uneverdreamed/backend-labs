094194e027e409137a251b9a87f4ee30193a12bf
uneverdreamed <ma2005f@gmail.com>
2026-02-20 10:26:05 +0300

    создан список пользователей

--- backendLab1/lab2/Program.cs
+++ backendLab1/lab2/Program.cs
@@ -1,8 +1,17 @@
+using lab2;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+var users = new List<User>
+{
+    new User { Id = 1, Name = "Имя1", Age = 21, Email = "imya1@pochta.com"},
+    new User { Id = 2, Name = "Имя2", Age = 31, Email = "imya2@pochta.com"},
+    new User { Id = 3, Name = "Имя3", Age = 41, Email = "imya3@pochta.com"}
+
+};
 
 app.Run();
