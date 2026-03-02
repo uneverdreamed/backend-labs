05f631d758369dc79840aa66e7de7b60181713cb
uneverdreamed <ma2005f@gmail.com>
2026-03-02 14:30:10 +0300

    4 | models | student | создана папка для моделей данных, создана модель данных student

---
+++ backendLab1/laba4/models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace laba4.models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Group { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
