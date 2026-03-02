173d675992c8d72a8da6913fe930b8389ec98000
uneverdreamed <ma2005f@gmail.com>
2026-03-02 14:41:35 +0300

    4 | models | реализован dto для обновления студента

--- backendLab1/laba4/models/StudentDTO.cs
+++ backendLab1/laba4/models/StudentDTO.cs
@@ -12,4 +12,15 @@ namespace laba4.models
         [StringLength(20, MinimumLength = 2, ErrorMessage = "Название группы должно быть от 2 до 20 символов")]
         public string Group { get; set; } = string.Empty;
     }
+
+    public class UpdateStudentDto
+    {
+        [Required(ErrorMessage = "Имя обязательно")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Имя должно быть от 2 до 100 символов")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Группа обязательна")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Название группы должно быть от 2 до 20 символов")]
+        public string Group { get; set; } = string.Empty;
+    }
 }
