57fe25f29eb1d52109ea78224d1826f5b6fe48b6
uneverdreamed <ma2005f@gmail.com>
2026-03-15 14:12:34 +0300

    5 | models | добавлены поля в модель курса (id, name, slug (короткое текстовое имя), guid (уникальный идентификатор))

--- backendLab1/laba4/models/Course.cs
+++ backendLab1/laba4/models/Course.cs
@@ -2,5 +2,10 @@
 {
     public class Course
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty; 
+        public int Year { get; set; }
+        public Guid Guid { get; set; }
     }
 }
