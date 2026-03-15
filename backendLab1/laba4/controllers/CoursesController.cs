4a74be27a39cb4e479b601652e4ce5486bf087e3
uneverdreamed <ma2005f@gmail.com>
2026-03-15 14:20:26 +0300

    5 | controllers | созданы 3 курса в классе CourseController

--- backendLab1/laba4/controllers/CoursesController.cs
+++ backendLab1/laba4/controllers/CoursesController.cs
@@ -1,6 +1,17 @@
-﻿namespace laba4.controllers
+﻿using laba4.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laba4.controllers
 {
-    public class CoursesController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
     {
+        private static readonly List<Course> _courses = new()
+        {
+            new Course { Id = 13672, Name = "Backend-разработка", Slug = "back-1372", Year = 2024, Guid = Guid.NewGuid() },
+            new Course { Id = 13377, Name = "Шаблоны проектирования", Slug = "despatterns-1377", Year = 2025, Guid = Guid.NewGuid() },
+            new Course { Id = 13533, Name = "Веб-программирование и дизайн", Slug = "webdev-1333", Year = 2026, Guid = Guid.NewGuid() }
+        };  
     }
 }
