70e08e1a9736baf911270bae5bd88dee7a544148
uneverdreamed <ma2005f@gmail.com>
2026-03-22 10:39:22 +0300

    6 | models | создан класс student

---
+++ backendLab1/laba6/models/student.cs
@@ -0,0 +1,11 @@
+﻿namespace laba6.models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Group { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
