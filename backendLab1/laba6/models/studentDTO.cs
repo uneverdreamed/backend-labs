069248eceae430cd117d6cd35db6a83e51de9378
uneverdreamed <ma2005f@gmail.com>
2026-03-22 10:41:33 +0300

    6 | models | создан класс studentDTO

---
+++ backendLab1/laba6/models/studentDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laba6.models
+{
+    public class CreateStudentDto
+    {
+        [Required(ErrorMessage = "Имя обязательно")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Имя должно быть от 2 до 100 символов")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Группа обязательна")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Название группы должно быть от 2 до 20 символов")]
+        public string Group { get; set; } = string.Empty;
+    }
+
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Group { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
