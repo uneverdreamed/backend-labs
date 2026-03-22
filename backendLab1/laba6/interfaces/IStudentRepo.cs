a1fe87e9696e4d7bf546b96dd29b64c201edc8ee
uneverdreamed <ma2005f@gmail.com>
2026-03-22 11:09:45 +0300

    6 | interfaces | создан интерфейс репозитория, отвечающий только за хранение данных в памяти

---
+++ backendLab1/laba6/interfaces/IStudentRepo.cs
@@ -0,0 +1,11 @@
+﻿using laba6.models;
+
+namespace laba6.interfaces
+{
+    public interface IStudentRepo
+    {
+        IEnumerable<student> GetAll();
+        student? GetById(int id);
+        student Add(student student);
+    }
+}
