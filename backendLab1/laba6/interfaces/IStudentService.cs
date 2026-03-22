d4fa2a9e63054e626cc20b5144203e5005c89975
uneverdreamed <ma2005f@gmail.com>
2026-03-22 11:06:20 +0300

    6 | interfaces | создан интерфейс сервера, который работает с DTO и скрывает детали хранения от контроллера

---
+++ backendLab1/laba6/interfaces/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using laba6.models;
+
+namespace laba6.interfaces
+{
+    public interface IStudentService
+    {
+        IEnumerable<StudentDto> GetAll();
+        StudentDto? GetById(int id);
+        StudentDto Create(CreateStudentDto dto);
+    }
+}
