6d3bf5d510593327281af00aad893b6735f191c8
uneverdreamed <ma2005f@gmail.com>
2026-03-29 19:12:04 +0300

    7 | middleware | создан EndpointTimingMiddleware, который измеряет длительность выполнения логики эндпоинта (Stopwatch), после await next() добавляет заголовок X-Endpoint-Elapsed-Ms (целое число миллисекунд)

---
+++ backendLab1/laba7/middleware/EndpointTimingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace laba7.middleware
+{
+    public class EndpointTimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public EndpointTimingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew(); // запуск таймера до передачи управления следующему компоненту
+
+
+            // все что находится ДО next() выполняется при входе запроса
+            await _next(context);
+            // все что находится ПОСЛЕ next() выполняется когда ответ уже сформирован
+            stopwatch.Stop();
+
+            // добавление заголовка с временем выполнения в миллисекундах
+            // используется TryAdd (если заголовок уже отправлен, не выбрасывается исключение)
+            context.Response.Headers.TryAdd("X-Endpoint-Elapsed-Ms", stopwatch.ElapsedMilliseconds.ToString());
+        }
+    }
+}
