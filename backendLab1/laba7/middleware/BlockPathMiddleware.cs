df7c7c4740ddde7ed9634b4131f537b0ea9860f8
uneverdreamed <ma2005f@gmail.com>
2026-03-29 19:15:20 +0300

    7 | middleware | создан BlockPathMiddleware, в котором если путь начинается с /blocked, сразу возвращает 403 Forbidden, иначе вызывает await next()

---
+++ backendLab1/laba7/middleware/BlockPathMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace laba7.middleware
+{
+    public class BlockPathMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BlockPathMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // проверка пути запроса (если начинается с /blocked, сразу возвращается 403)
+            // next() не вызывается, конвейер прерывается здесь
+            if (context.Request.Path.StartsWithSegments("/blocked"))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Access denied: this path is blocked.");
+                return;
+            }
+            // если путь не заблокирован, то управление передается дальше по конвейеру
+            await _next(context);
+        }
+    }
+}
