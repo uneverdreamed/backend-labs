b2415c1132267c1c131e72e5cd3cc0c16cdad0b8
uneverdreamed <ma2005f@gmail.com>
2026-03-29 19:08:26 +0300

    7 | middleware | создан RequestTraceMiddleware для создания идентификатора запроса и добавляет заголовок в ответ

---
+++ backendLab1/laba7/middleware/RequestTraceMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace laba7.middleware
+{
+    public class RequestTraceMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestTraceMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var traceId = Guid.NewGuid().ToString(); // генерация уникального TraceId для каждого запроса
+
+
+            // сохранение TraceId в HttpContext.Items, чтобы он был доступен всем middleware и эндпоинтам в рамках запроса
+            context.Items["TraceId"] = traceId;
+
+            context.Response.Headers["X-Trace-Id"] = traceId; // добавление TraceId в заголовок ответа, чтобы клиент мог его увидеть
+
+            await _next(context); // передача управления следующему компоненту конвейера
+
+        }
+    }
+}
