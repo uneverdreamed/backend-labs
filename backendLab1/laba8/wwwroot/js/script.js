5024a02736a531083b6db870a00778b2cb1d46db
uneverdreamed <ma2005f@gmail.com>
2026-04-05 12:05:54 +0300

    8 | js | создан файл script.js - добавляет обработчик нажатия кнопки и обновляет счетчик кликов

---
+++ backendLab1/laba8/wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById('btn');
+    const output = document.getElementById('output');
+
+    let count = 0;
+
+    btn.addEventListener('click', function () {
+        count++;
+        output.textContent = `Кнопка нажата ${count} раз(а). JavaScript работает!`;
+    });
+});
