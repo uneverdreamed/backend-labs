6273359379f111f1ac19f69016b709d45ccf8ecc
uneverdreamed <ma2005f@gmail.com>
2026-04-19 09:18:42 +0300

    11 | исправление ошибки с циклическими ссылками при сериализации

--- backendLab1/laba11/Program.cs
+++ backendLab1/laba11/Program.cs
@@ -3,7 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // игнорировать циклические ссылки при сериализации
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
