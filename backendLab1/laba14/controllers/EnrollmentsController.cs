cfee71d8f44572a9f30ce4e3823bbdbe76fa3d3a
uneverdreamed <ma2005f@gmail.com>
2026-05-12 15:33:30 +0300

    14 | изменен namespace

--- backendLab1/laba14/controllers/EnrollmentsController.cs
+++ backendLab1/laba14/controllers/EnrollmentsController.cs
@@ -1,10 +1,10 @@
-﻿using laba13.data;
-using laba13.DTOs;
-using laba13.models;
+﻿using laba14.data;
+using laba14.DTOs;
+using laba14.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace laba13.controllers
+namespace laba14.controllers
 {
     [ApiController]
     [Route("api/[controller]")]
