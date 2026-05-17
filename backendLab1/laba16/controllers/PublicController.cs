da9cc162ef8bd9705016136854d0e99957571508
uneverdreamed <ma2005f@gmail.com>
2026-05-17 11:32:12 +0300

    16 | controllers | добавлен PublicController для проверки работоспособности эндпоинтов без [Authorize] для всех

---
+++ backendLab1/laba16/controllers/PublicController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace laba16.Controllers;
+
+// Открытый контроллер - доступен без токена
+// Используется для проверки, что эндпоинты без [Authorize] работают для всех
+[ApiController]
+[Route("api/[controller]")]
+public class PublicController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            message = "Это открытый эндпоинт, токен не требуется",
+            time = DateTime.Now
+        });
+    }
+}
