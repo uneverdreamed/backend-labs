e7dde0d750def63c1c8e4f5cb8be4ff4d421063c
uneverdreamed <ma2005f@gmail.com>
2026-05-17 10:50:57 +0300

    16 | скопирована папка models из laba11

---
+++ backendLab1/laba16/models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace laba11.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Credits { get; set; }
+
+        // навигационное свойство (один курс может иметь много записей студентов)
+        [JsonIgnore]
+        public ICollection<Enroll> Enrolls { get; set; } = new List<Enroll>();
+    }
+}
