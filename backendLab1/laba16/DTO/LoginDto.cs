772910d9fb8230b72e653a626e2c0ec3bc3859d0
uneverdreamed <ma2005f@gmail.com>
2026-05-17 11:10:52 +0300

    16 | dto | добавлен LoginDto для приема данных при попытке входа. используется отдельно от модели User

---
+++ backendLab1/laba16/DTO/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace laba16.DTO;
+
+// DTO для приёма данных при попытке входа
+// Используется отдельно от модели User, чтобы не передавать через API лишних полей
+public class LoginDto
+{
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
